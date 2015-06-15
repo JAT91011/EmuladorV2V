15960d2c4449562ec63c28061e4bdd824b6ce109
Jordan Aranda <jordan.aranda@me.com>
2015-06-15 21:03:41 +0200

    Se crea formulario para probar bluetooth

---
+++ SimuladorV2V/Temporal/frmBluetoothTester.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace SimuladorV2V.Temporal
+{
+    partial class frmBluetoothTester
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ManiXButton.Office2010Blue office2010Blue3 = new ManiXButton.Office2010Blue();
+            this.lblSalida = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new ManiXButton.MyButton();
+            this.lblEntrada = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblPuertoSerie = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lblVelocidad = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lblSalida
+            // 
+            this.lblSalida.AutoSize = true;
+            this.lblSalida.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalida.ForeColor = System.Drawing.Color.LightGray;
+            this.lblSalida.Location = new System.Drawing.Point(15, 70);
+            this.lblSalida.Name = "lblSalida";
+            this.lblSalida.Size = new System.Drawing.Size(52, 18);
+            this.lblSalida.TabIndex = 0;
+            this.lblSalida.Text = "Salida:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(405, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnEnviar
+            // 
+            office2010Blue3.BorderColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(72)))), ((int)(((byte)(161)))));
+            office2010Blue3.BorderColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(135)))), ((int)(((byte)(228)))));
+            office2010Blue3.ButtonMouseOverColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(199)))), ((int)(((byte)(87)))));
+            office2010Blue3.ButtonMouseOverColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(243)))), ((int)(((byte)(215)))));
+            office2010Blue3.ButtonMouseOverColor3 = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(225)))), ((int)(((byte)(137)))));
+            office2010Blue3.ButtonMouseOverColor4 = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(249)))), ((int)(((byte)(224)))));
+            office2010Blue3.ButtonNormalColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(72)))), ((int)(((byte)(161)))));
+            office2010Blue3.ButtonNormalColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(135)))), ((int)(((byte)(228)))));
+            office2010Blue3.ButtonNormalColor3 = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(97)))), ((int)(((byte)(181)))));
+            office2010Blue3.ButtonNormalColor4 = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(125)))), ((int)(((byte)(219)))));
+            office2010Blue3.ButtonSelectedColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(199)))), ((int)(((byte)(87)))));
+            office2010Blue3.ButtonSelectedColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(243)))), ((int)(((byte)(215)))));
+            office2010Blue3.ButtonSelectedColor3 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(229)))), ((int)(((byte)(117)))));
+            office2010Blue3.ButtonSelectedColor4 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(216)))), ((int)(((byte)(107)))));
+            office2010Blue3.HoverTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(57)))), ((int)(((byte)(91)))));
+            office2010Blue3.SelectedTextColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(57)))), ((int)(((byte)(91)))));
+            office2010Blue3.TextColor = System.Drawing.Color.White;
+            this.btnEnviar.ColorTable = office2010Blue3;
+            this.btnEnviar.Location = new System.Drawing.Point(492, 70);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(73, 24);
+            this.btnEnviar.TabIndex = 2;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.Theme = ManiXButton.Theme.MSOffice2010_BLUE;
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            // 
+            // lblEntrada
+            // 
+            this.lblEntrada.AutoSize = true;
+            this.lblEntrada.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEntrada.ForeColor = System.Drawing.Color.LightGray;
+            this.lblEntrada.Location = new System.Drawing.Point(12, 110);
+            this.lblEntrada.Name = "lblEntrada";
+            this.lblEntrada.Size = new System.Drawing.Size(63, 18);
+            this.lblEntrada.TabIndex = 3;
+            this.lblEntrada.Text = "Entrada:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(81, 110);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(484, 201);
+            this.textBox2.TabIndex = 4;
+            // 
+            // lblPuertoSerie
+            // 
+            this.lblPuertoSerie.AutoSize = true;
+            this.lblPuertoSerie.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPuertoSerie.ForeColor = System.Drawing.Color.LightGray;
+            this.lblPuertoSerie.Location = new System.Drawing.Point(15, 30);
+            this.lblPuertoSerie.Name = "lblPuertoSerie";
+            this.lblPuertoSerie.Size = new System.Drawing.Size(56, 18);
+            this.lblPuertoSerie.TabIndex = 5;
+            this.lblPuertoSerie.Text = "Puerto:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(81, 31);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(252, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // lblVelocidad
+            // 
+            this.lblVelocidad.AutoSize = true;
+            this.lblVelocidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVelocidad.ForeColor = System.Drawing.Color.LightGray;
+            this.lblVelocidad.Location = new System.Drawing.Point(339, 30);
+            this.lblVelocidad.Name = "lblVelocidad";
+            this.lblVelocidad.Size = new System.Drawing.Size(56, 18);
+            this.lblVelocidad.TabIndex = 7;
+            this.lblVelocidad.Text = "Puerto:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(401, 31);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(164, 21);
+            this.comboBox2.TabIndex = 8;
+            // 
+            // frmBluetoothTester
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(577, 323);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.lblVelocidad);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lblPuertoSerie);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblEntrada);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblSalida);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmBluetoothTester";
+            this.Text = "Bluetooth";
+            this.Load += new System.EventHandler(this.frmBluetoothTester_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSalida;
+        private System.Windows.Forms.TextBox textBox1;
+        private ManiXButton.MyButton btnEnviar;
+        private System.Windows.Forms.Label lblEntrada;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblPuertoSerie;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lblVelocidad;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
---
+++ SimuladorV2V/Temporal/frmBluetoothTester.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimuladorV2V.Temporal
+{
+    public partial class frmBluetoothTester : Form
+    {
+        public frmBluetoothTester()
+        {
+            InitializeComponent();
+        }
+
+        private void frmBluetoothTester_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
